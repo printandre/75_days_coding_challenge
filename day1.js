818003730f0dae51057fc32e06b7a2c71745ef84
printandre <andreasalvatore.romano@gmail.com>
2024-05-16 12:57:19 +0200

    comment modified

--- day1.js
+++ day1.js
@@ -1,10 +1,10 @@
 /* 
-*     Day 1: Concatenation of Array
+*    Day 1: Concatenation of Array
 *    Given an integer array nums of length n, you want to
-*    create an array ans of length 2n where ans[i] == nums[i]
-*    and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
-*    Specifically, ans is the concatenation of two nums arrays.
-*    Return the array ans 
+*    create an array newArr of length 2n where newArr[i] == nums[i]
+*    and newArr[i + n] == nums[i] for 0 <= i < n (0-indexed).
+*    Specifically, newArr is the concatenation of two nums arrays.
+*    Return the array newArr 
 */
 
 function concat(arr){
